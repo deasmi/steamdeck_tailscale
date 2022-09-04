c74298bdd27a1ad581dd12dbf68296d0f1a2353c
Dean Smith <dean@zelotus.com>
2022-09-04 12:36:44 +0100

    First version

---
+++ README.md
@@ -0,0 +1,23 @@
+Steam Deck tailscale setup script
+=================================
+
+This script will setup tailscale on your steam deck to be persistent, that is
+it will start on ever boot.
+
+Clone this repo into a directory then run
+`./tailscale_deck_setup.sh`
+You will need to enter you password as file ownership/suid changes need root
+
+It uses systemctl user mode
+
+Notes/FAQ
+
+You are using root suid files, isn't that a bad idea?
+Yes, if you are running enterprise linux that's probably not the best
+idea, this is a Steam Deck.
+The executables are only readable/exectuable by members of the deck group, of which
+the deck user is the only members.
+
+Shouldn't you be using ${HOME} in those sed commands?
+Probably, but I couldn't be bothered to work out how to escape the shell expansion
+
---
+++ tailscale_deck_setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dir="$(mktemp -d)"
+pushd .
+echo "${dir}"
+cd "${dir}"
+
+tarball="$(curl 'https://pkgs.tailscale.com/stable/?mode=json' | jq -r .Tarballs.amd64)"
+version="$(echo ${tarball} | cut -d_ -f2)"
+
+curl "https://pkgs.tailscale.com/stable/${tarball}" -o tailscale.tgz
+
+mkdir -p tailscale/usr/{bin,sbin,lib/{systemd/system,extension-release.d}}
+tar xzf tailscale.tgz
+
+mkdir -p ~/bin
+mkdir -p ~/.config/default
+
+cp -vrf "tailscale_${version}_amd64"/tailscale ~/bin/tailscale
+sudo chown root:deck ~/bin/tailscale
+sudo chmod 4750 ~/bin/tailscale
+cp -vrf "tailscale_${version}_amd64"/tailscaled ~/bin/tailscaled
+sudo chown root:deck ~/bin/tailscaled
+sudo chmod 4750 ~/bin/tailscaled
+cp -vrf "tailscale_${version}_amd64"/systemd/tailscaled.service ~/.config/systemd/user/tailscaled.service
+cp -vrf "tailscale_${version}_amd64"/systemd/tailscaled.defaults ~/.config/default/tailscale
+
+sed -i 's/--port.*//g' ~/.config/systemd/user/tailscaled.service
+sed -i 's/\/etc\/default/\~\/.config\/default\/tailscale/g' ~/.config/systemd/user/tailscaled.service
+sed -i 's/\/usr\/sbin/\/home\/deck\/bin/g' ~/.config/systemd/user/tailscaled.service
+sed -i 's/\/usr\/bin/\/home\/deck\/bin/g' ~/.config/systemd/user/tailscaled.service
+sed -i 's/multi-user.target/default.target/g' ~/.config/systemd/user/tailscaled.service
+
+popd
+rm -rf "${dir}"
